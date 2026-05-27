f7465954a350f7989b5630f51e74326fc63a532f
Ryangateley-cmyk <Ryangateley@outlook.com>
2026-05-28 00:36:45 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Im Literally The GOAT What Can I Say?",
         // Activity type number (0 = Playing).
         type: 0, 
       },
